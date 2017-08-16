a95aa1f7071385bb3fbc1c81a7899f946b5d1163
d4rkz3r0 <d4rkz3r0@MacBookPro.local>
2017-08-15 20:07:32 -0400

    Login UI elements created.

---
+++ SlackClone/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  SlackClone
+//
+//  Created by Steve Kerney on 8/15/17.
+//  Copyright © 2017 Steve Kerney. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: NSViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    }
+}
+
